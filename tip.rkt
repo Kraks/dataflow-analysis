b462e5978eaad3d81969daf7af3ea784fe1e1785
Guannan Wei <kiss.kraks@gmail.com>
2016-11-04 18:32:48 -0600

    add transparent

--- tip.rkt
+++ tip.rkt
@@ -2,13 +2,13 @@
 
 (provide (all-defined-out))
 
+(struct Return ([e : Expr]) #:transparent)
 (struct Fun ([name : Id]
              [args : (Listof Id)]
              [locals : (Listof Id)]
              [body : Stmt]
-             [ret : Expr]))
-
-
+             [ret : Return])
+  #:transparent)
 
 (define-type Expr (U Int Id Plus
                      Minus Mult Div
@@ -19,48 +19,29 @@
 (define-type Id Symbol)
 (define-type Int Integer)
 
-(struct Plus ([lhs : Expr] [rhs : Expr]))
-(struct Minus ([lhs : Expr] [rhs : Expr]))
-(struct Mult ([lhs : Expr] [rhs : Expr]))
-(struct Div ([lhs : Expr] [rhs : Expr]))
-(struct Greater ([lhs : Expr] [rhs : Expr]))
-(struct Equal ([lhs : Expr] [rhs : Expr]))
+(struct Plus ([lhs : Expr] [rhs : Expr]) #:transparent)
+(struct Minus ([lhs : Expr] [rhs : Expr]) #:transparent)
+(struct Mult ([lhs : Expr] [rhs : Expr]) #:transparent)
+(struct Div ([lhs : Expr] [rhs : Expr]) #:transparent)
+(struct Greater ([lhs : Expr] [rhs : Expr]) #:transparent)
+(struct Equal ([lhs : Expr] [rhs : Expr]) #:transparent)
 
 (struct Input ())
 
-(struct App ([fun : Expr] [args : (Listof Expr)]))
+(struct App ([fun : Expr] [args : (Listof Expr)]) #:transparent)
 
-(struct AddrOf ([var : Id]))
+(struct AddrOf ([var : Id]) #:transparent)
 (struct Malloc ())
-(struct DeRef ([e : Expr]))
+(struct DeRef ([e : Expr]) #:transparent)
 (struct Null ())
 
 (define-type Stmt (U Assign Output Seq
                      If NoOp While))
-(struct Assign ([id : (U Id DeRef)] [e : Expr]))
-(struct Output ([e : Expr]))
-(struct Seq ([stmts : (Listof Stmt)]))
-(struct If ([cnd : Expr] [thn : Stmt] [els : Stmt]))
+(struct Assign ([id : (U Id DeRef)] [e : Expr]) #:transparent)
+(struct Output ([e : Expr]) #:transparent)
+(struct Seq ([stmts : (Listof Stmt)]) #:transparent)
+(struct If ([cnd : Expr] [thn : Stmt] [els : Stmt]) #:transparent)
 (struct NoOp ())
-(struct While ([cnd : Expr] [body : Stmt]))
+(struct While ([cnd : Expr] [body : Stmt]) #:transparent)
 
 (define-type Program (Listof Fun))
-
-;;;;;;;;;;;;;;;;;;;;;;;;
-
-; Example programs
-
-(define rec '(rec (n)
-               (var f)
-               (if (== n 0)
-                   (:= f 1)
-                   (:= f (* n (rec (- n 1)))))
-               (return f)))
-
-(define frec (Fun 'rec '(n) '(f)
-                  (If (Equal 'n 0)
-                      (Assign 'f 1)
-                      (Assign 'f (Mult 'n (App 'rec (list (Minus 'n 1))))))
-                  'f))
-
-

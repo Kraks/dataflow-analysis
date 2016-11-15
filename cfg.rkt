80d095ddefed80cfebb0bcd7b1095697bf7a15b3
Guannan Wei <kiss.kraks@gmail.com>
2016-11-14 20:23:41 -0700

    misc

--- cfg.rkt
+++ cfg.rkt
@@ -1,8 +1,13 @@
 #lang typed/racket
 
+;; Intraprocedure control flow graph, transformed from AST
+
 (require "tip.rkt")
 
 (struct Node ([body : (U Stmt Expr)]
               [pred : (Listof Node)]
               [succ : (Listof Node)]))
 (struct Edge ([from : Node] [to : Node]))
+
+;(define-type Stmt (U Assign Output Seq If NoOp While))
+;(define (ast->cfg stmt)

712c8e10cc929d0becab7ebe4bb2d4692ad41314
Guannan Wei <kiss.kraks@gmail.com>
2017-05-20 16:52:17 -0700

    refactor

--- parser.rkt
+++ parser.rkt
@@ -8,19 +8,16 @@
 ; sexp -> Stmt
 (define (parse-stmt s)
   (match s
-    [`(:= ,(? symbol? id) ,e) (Assign id (parse-expr e))]
-    [`(:= (* ,l) ,e) (Assign (DeRef (parse-expr l))
-                             (parse-expr e))]
-    [`(output ,e) (Output (parse-expr e))]
-    [`(if ,cnd ,thn ,els) (If (parse-expr cnd)
+    [`{:= ,id ,e} (Assign (parse-expr id) (parse-expr e))]
+    [`{output ,e} (Output (parse-expr e))]
+    [`{if ,cnd ,thn ,els} (If (parse-expr cnd)
                               (parse-stmt thn)
                               (parse-stmt els))]
-    [`(while ,cnd ,body) (While (parse-expr cnd)
+    [`{while ,cnd ,body} (While (parse-expr cnd)
                                 (parse-stmt body))]
-    [`(noop) (NoOp)]
-    [`(return ,e) (Return (parse-expr e))]
-    [`(,stmts ...)
-     (map parse-stmt stmts)]
+    [`{noop} (NoOp)]
+    [`{return ,e} (Return (parse-expr e))]
+    [`{,stmts ...} (map parse-stmt stmts)]
     [else (error 'parse-stmt "can not parse statement")]))
 
 ; sexp -> Expr
@@ -28,66 +25,69 @@
   (match e
     [(? symbol? s) s]
     [(? integer? i) i]
-    [`(+ ,lhs ,rhs) (Plus (parse-expr lhs)
+    [`{+ ,lhs ,rhs} (Plus (parse-expr lhs)
                           (parse-expr rhs))]
-    [`(- ,lhs ,rhs) (Minus (parse-expr lhs)
+    [`{- ,lhs ,rhs} (Minus (parse-expr lhs)
                            (parse-expr rhs))]
-    [`(* ,lhs ,rhs) (Mult (parse-expr lhs)
+    [`{* ,lhs ,rhs} (Mult (parse-expr lhs)
                           (parse-expr rhs))]
-    [`(/ ,lhs ,rhs) (Div (parse-expr lhs)
+    [`{/ ,lhs ,rhs} (Div (parse-expr lhs)
                          (parse-expr rhs))]
-    [`(> ,lhs ,rhs) (Greater (parse-expr lhs)
+    [`{> ,lhs ,rhs} (Greater (parse-expr lhs)
                              (parse-expr rhs))]
-    [`(== ,lhs ,rhs) (Equal (parse-expr lhs)
+    [`{== ,lhs ,rhs} (Equal (parse-expr lhs)
                             (parse-expr rhs))]
-    [`(input) (Input)]
-    [`(null) (Null)]
-    [`(& ,(? symbol? id)) (AddrOf id)]
-    [`(* ,e) (DeRef (parse-expr e))]
-    [`(,fun ,args ...) (App (parse-expr fun)
+    [`{input} (Input)]
+    [`{null} (Null)]
+    [`{& ,(? symbol? id)} (AddrOf id)]
+    [`{* ,e} (DeRef (parse-expr e))]
+    [`{,fun ,args ...} (App (parse-expr fun)
                             (map parse-expr args))]
     [else (error 'parse-expr "can not parse expression")]))
 
 ; sexp -> Fun
 (define (parse-function f)
   (match f
-    [`(,fname (,vars ...) (var ,locals ...) ,stmts)
+    [`{,fname {,vars ...} {var ,locals ...} ,stmts}
      (Fun fname vars locals (parse-stmt stmts))]
+    [`{,fname {,vars ...} {} ,stmts}
+     (Fun fname vars '() (parse-stmt stmts))]
     [else (error 'parse-function "can not parse function")]))
 
 ;;;;;;;;;;;;;;;;;
 
-(check-equal? (parse-stmt '(while (== 1 2) (output 3)))
+(check-equal? (parse-stmt '{while {== 1 2} {output 3}})
               (While (Equal 1 2) (Output 3)))
-(check-equal? (parse-stmt '((:= a 3)))
+
+(check-equal? (parse-stmt '{{:= a 3}})
               (list (Assign 'a 3)))
-(check-equal? (parse-stmt '((:= a 3) (:= b 4) (:= c (+ a b))))
+
+(check-equal? (parse-stmt '{{:= a 3} {:= b 4} {:= c {+ a b}}})
               (list (Assign 'a 3) (Assign 'b 4) (Assign 'c (Plus 'a 'b))))
-(check-equal? (parse-stmt '(if (== a b)
-                               ((:= a 3) (:= b 4))
-                               ((:= (* a) 4) (:= a 3))))
+
+(check-equal? (parse-stmt '{if {== a b}
+                               {{:= a 3} {:= b 4}}
+                               {{:= {* a} 4} {:= a 3}}})
               (If (Equal 'a 'b)
                   (list (Assign 'a 3) (Assign 'b 4))
                   (list (Assign (DeRef 'a) 4) (Assign 'a 3))))
-(check-equal? (parse-stmt '(while (> x 0)
-                                  (:= x (- x 1))))
+
+(check-equal? (parse-stmt '{while {> x 0}
+                                  {:= x {- x 1}}})
               (While (Greater 'x 0) (Assign 'x (Minus 'x 1))))
 
-(define rec '(rec (n)
-               (var f)
-               ((if (== n 0)
-                    (:= f 1)
-                    (:= f (* n (rec (- n 1)))))
-                (return f))))
-(define frec (Fun 'rec '(n) '(f)
-                  (list
-                   (If (Equal 'n 0)
-                       (Assign 'f 1)
-                       (Assign 'f (Mult 'n (App 'rec (list (Minus 'n 1))))))
-                   (Return 'f))))
-(check-equal? (parse-function rec) frec)
+(check-equal? (parse-function '{rec {n}
+                                 {var f}
+                                 {{if {== n 0} {:= f 1}
+                                      {:= f {* n {rec {- n 1}}}}}
+                                  {return f}}})
+              (Fun 'rec '(n) '(f)
+                   (list
+                    (If (Equal 'n 0)
+                        (Assign 'f 1)
+                        (Assign 'f (Mult 'n (App 'rec (list (Minus 'n 1))))))
+                    (Return 'f))))
 
-(check-equal? (parse-function '(add (x y)
-                                    (var)
-                                    (return (+ x y))))
+(check-equal? (parse-function '{add {x y} {}
+                                    {return {+ x y}}})
               (Fun 'add '(x y) '() (Return (Plus 'x 'y))))

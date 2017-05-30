51488c631d228278dd81288914b43589873fe994
Guannan Wei <kiss.kraks@gmail.com>
2017-05-30 13:52:25 -0600

    Refactor

--- parser.rkt
+++ parser.rkt
@@ -60,60 +60,59 @@
 ;;;;;;;;;;;;;;;;;
 
 (module+ test
+  (check-equal? (parse-stmt '{while {== 1 2} {output 3}})
+                (While (Equal 1 2) (Output 3)))
 
-(check-equal? (parse-stmt '{while {== 1 2} {output 3}})
-              (While (Equal 1 2) (Output 3)))
+  (check-equal? (parse-stmt '{{:= a 3}})
+                (Seq (list (Assign 'a 3))))
 
-(check-equal? (parse-stmt '{{:= a 3}})
-              (Seq (list (Assign 'a 3))))
+  (check-equal? (parse-stmt '{{:= a 3} {:= b 4} {:= c {+ a b}}})
+                (Seq (list (Assign 'a 3) (Assign 'b 4) (Assign 'c (Plus 'a 'b)))))
 
-(check-equal? (parse-stmt '{{:= a 3} {:= b 4} {:= c {+ a b}}})
-              (Seq (list (Assign 'a 3) (Assign 'b 4) (Assign 'c (Plus 'a 'b)))))
+  (check-equal? (parse-stmt '{if {== a b}
+                                 {{:= a 3} {:= b 4}}
+                                 {{:= {* a} 4} {:= a 3}}})
+                (If (Equal 'a 'b)
+                    (Seq (list (Assign 'a 3) (Assign 'b 4)))
+                    (Seq (list (Assign (DeRef 'a) 4) (Assign 'a 3)))))
 
-(check-equal? (parse-stmt '{if {== a b}
-                               {{:= a 3} {:= b 4}}
-                               {{:= {* a} 4} {:= a 3}}})
-              (If (Equal 'a 'b)
-                  (Seq (list (Assign 'a 3) (Assign 'b 4)))
-                  (Seq (list (Assign (DeRef 'a) 4) (Assign 'a 3)))))
+  (check-equal? (parse-stmt '{while {> x 0}
+                                    {:= x {- x 1}}})
+                (While (Greater 'x 0) (Assign 'x (Minus 'x 1))))
 
-(check-equal? (parse-stmt '{while {> x 0}
-                                  {:= x {- x 1}}})
-              (While (Greater 'x 0) (Assign 'x (Minus 'x 1))))
+  (check-equal? (parse-function '{rec {n}
+                                   {var f}
+                                   {{if {== n 0} {:= f 1}
+                                        {:= f {* n {rec {- n 1}}}}}
+                                    {return f}}})
+                (Fun 'rec '(n) '(f)
+                     (Seq (list
+                           (If (Equal 'n 0)
+                               (Assign 'f 1)
+                               (Assign 'f (Mult 'n (App 'rec (list (Minus 'n 1))))))
+                           (Return 'f)))))
 
-(check-equal? (parse-function '{rec {n}
-                                 {var f}
-                                 {{if {== n 0} {:= f 1}
-                                      {:= f {* n {rec {- n 1}}}}}
-                                  {return f}}})
-              (Fun 'rec '(n) '(f)
-                   (Seq (list
-                         (If (Equal 'n 0)
-                             (Assign 'f 1)
-                             (Assign 'f (Mult 'n (App 'rec (list (Minus 'n 1))))))
-                         (Return 'f)))))
+  (check-equal? (parse-function '{add {x y} {}
+                                      {return {+ x y}}})
+                (Fun 'add '(x y) '() (Return (Plus 'x 'y))))
 
-(check-equal? (parse-function '{add {x y} {}
-                                    {return {+ x y}}})
-              (Fun 'add '(x y) '() (Return (Plus 'x 'y))))
-
-(check-equal? (parse-function '{foo {p x}
-                                    {var f q}
-                                    {{if {== {* p} 0}
-                                         {:= f 1}
-                                         {{:= q {malloc}}
-                                          {:= {* q} {- {* q} 1}}
-                                          {:= f {* {* p} {{* x} q x}}}}}
-                                     {return f}}})
-              (Fun 'foo '(p x) '(f q)
-                   (Seq (list
-                         (If
-                          (Equal (DeRef 'p) 0)
-                          (Assign 'f 1)
-                          (Seq
-                           (list
-                            (Assign 'q (Malloc))
-                            (Assign (DeRef 'q) (Minus (DeRef 'q) 1))
-                            (Assign 'f (Mult (DeRef 'p) (App (DeRef 'x) '(q x)))))))
-                         (Return 'f)))))
-)
+  (check-equal? (parse-function '{foo {p x}
+                                      {var f q}
+                                      {{if {== {* p} 0}
+                                           {:= f 1}
+                                           {{:= q {malloc}}
+                                            {:= {* q} {- {* q} 1}}
+                                            {:= f {* {* p} {{* x} q x}}}}}
+                                       {return f}}})
+                (Fun 'foo '(p x) '(f q)
+                     (Seq (list
+                           (If
+                            (Equal (DeRef 'p) 0)
+                            (Assign 'f 1)
+                            (Seq
+                             (list
+                              (Assign 'q (Malloc))
+                              (Assign (DeRef 'q) (Minus (DeRef 'q) 1))
+                              (Assign 'f (Mult (DeRef 'p) (App (DeRef 'x) '(q x)))))))
+                           (Return 'f)))))
+  )

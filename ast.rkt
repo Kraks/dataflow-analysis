a3271105f9b88ff250e743ba222826985b492d26
Guannan Wei <kiss.kraks@gmail.com>
2017-06-01 15:12:10 -0600

    Add auxiliary functions

--- ast.rkt
+++ ast.rkt
@@ -44,3 +44,31 @@ Program ::= Fun*
 |#
 
 (struct Program (funs))
+
+;; Auxiliary functions
+
+(define not-constant?
+  (λ (x) (and (not (integer? x)) (not (boolean? x)))))
+
+(define (expr-contains-var? expr var)
+  (match expr
+    [(? integer?) #f]
+    [(? boolean? x) #f]
+    [(? symbol? x) (eq? x var)]
+    [(Plus l r) (or (expr-contains-var? l var)
+                    (expr-contains-var? r var))]
+    [(Minus l r) (or (expr-contains-var? l var)
+                     (expr-contains-var? r var))]
+    [(Mult l r) (or (expr-contains-var? l var)
+                    (expr-contains-var? r var))]
+    [(Div l r) (or (expr-contains-var? l var)
+                   (expr-contains-var? r var))]
+    [(Greater l r) (or (expr-contains-var? l var)
+                       (expr-contains-var? r var))]
+    [(Equal l r) (or (expr-contains-var? l var)
+                     (expr-contains-var? r var))]
+    ;; TODO: handle function pointer
+    [(App f args) (ormap (λ (e) (expr-contains-var? e var)) args)]
+    [(AddrOf x) (eq? x var)]
+    [(DeRef e) (expr-contains-var? e var)]
+    [else #f]))

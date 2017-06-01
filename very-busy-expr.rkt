a77d3a39686835f780b69367ae48c161a558979e
Guannan Wei <kiss.kraks@gmail.com>
2017-06-01 15:12:37 -0600

    Refactor code

--- very-busy-expr.rkt
+++ very-busy-expr.rkt
@@ -12,32 +12,6 @@
 
 (require rackunit)
 
-(define (expr-contains-var? expr var)
-  (match expr
-    [(? integer?) #f]
-    [(? boolean? x) #f]
-    [(? symbol? x) (eq? x var)]
-    [(Plus l r) (or (expr-contains-var? l var)
-                    (expr-contains-var? r var))]
-    [(Minus l r) (or (expr-contains-var? l var)
-                     (expr-contains-var? r var))]
-    [(Mult l r) (or (expr-contains-var? l var)
-                    (expr-contains-var? r var))]
-    [(Div l r) (or (expr-contains-var? l var)
-                   (expr-contains-var? r var))]
-    [(Greater l r) (or (expr-contains-var? l var)
-                       (expr-contains-var? r var))]
-    [(Equal l r) (or (expr-contains-var? l var)
-                     (expr-contains-var? r var))]
-    ;; TODO: handle function pointer
-    [(App f args) (ormap (λ (e) (expr-contains-var? e var)) args)]
-    [(AddrOf x) (eq? x var)]
-    [(DeRef e) (expr-contains-var? e var)]
-    [else #f]))
-
-(define not-constant?
-  (λ (x) (and (not (integer? x)) (not (boolean? x)))))
-
 (define very-busy-analysis
   (Analysis
    ; direction
